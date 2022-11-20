75a38893d5e1aaedc2feacf7e2f2a53e170c8016
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-11-20 11:08:13 +0100

    Update Adapter example

--- structural-paterns/adapter.ts
+++ structural-paterns/adapter.ts
@@ -1,11 +1,11 @@
-const books = [
-  { title: "Book 1", author: "Author 1" },
-  { title: "Book 2", author: "Author 2" },
-  { title: "Book 3", author: "Author 3" },
-];
-
 class LibraryCSV {
   getBooks() {
+    const books = [
+      { title: "Book 1", author: "Author 1" },
+      { title: "Book 2", author: "Author 2" },
+      { title: "Book 3", author: "Author 3" },
+    ];
+
     return (
       "title,author\r\n" +
       books.map((book) => `${book.title},${book.author}`).join("\r\n")
@@ -15,6 +15,12 @@ class LibraryCSV {
 
 class LibraryJSON {
   getBooks() {
+    const books = [
+      { title: "Book 1", author: "Author 1" },
+      { title: "Book 2", author: "Author 2" },
+      { title: "Book 3", author: "Author 3" },
+    ];
+
     return JSON.stringify(books);
   }
 }
