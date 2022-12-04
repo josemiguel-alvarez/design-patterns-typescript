8ae025b020be3843725afc27dcb0025dad00f3fe
José Miguel Álvarez Vañó <jm.alvarez.vano@gmail.com>
2022-12-04 21:57:32 +0100

    Add Composite pattern

---
+++ structural-paterns/composite.ts
@@ -0,0 +1,57 @@
+interface Component {
+  render: () => void;
+}
+
+class SimpleText implements Component {
+  private text: string;
+
+  constructor(text: string) {
+    this.text = text;
+  }
+
+  public render() {
+    console.log("Rendering text:", this.text);
+  }
+}
+
+class SimpleImage implements Component {
+  private src: string;
+
+  constructor(src: string) {
+    this.src = src;
+  }
+
+  public render() {
+    console.log("Rendering image:", this.src);
+  }
+}
+
+class Block implements Component {
+  private children: Component[] = [];
+
+  public add(child: Component): void {
+    this.children.push(child);
+  }
+
+  public render() {
+    this.children.forEach((child) => child.render());
+  }
+}
+
+// Simple block example
+const block = new Block();
+block.add(new SimpleText("Hello"));
+block.add(new SimpleImage("https://example.com/image.png"));
+
+block.render();
+
+// Nested blocks example
+const nestedBlock = new Block();
+nestedBlock.add(new SimpleText("Hello, check the following image:"));
+nestedBlock.add(new SimpleImage("https://example.com/image.png"));
+
+const mainBlock = new Block();
+mainBlock.add(nestedBlock);
+mainBlock.add(new SimpleText("Thanks for reading!"));
+
+mainBlock.render();
